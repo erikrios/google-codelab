3f277d21b38201834eb57a1871da0296eb0604cd
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-24 20:19:29 +0700

    Create MainViewModel.kt

---
+++ MyInstrumentTest/app/src/main/java/com/erikriosetiawan/myinstrumenttest/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.erikriosetiawan.myinstrumenttest
+
+internal class MainViewModel(private val cuboidModel: CuboidModel) {
+
+    val circumference: Double get() = cuboidModel.circumference
+    val surfaceArea: Double get() = cuboidModel.surfaceArea
+    val volume: Double get() = cuboidModel.volume
+    fun save(l: Double, w: Double, h: Double) {
+        cuboidModel.save(l, w, h)
+    }
+}
