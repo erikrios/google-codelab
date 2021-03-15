b6ade95822de768a19c02ee057a274871073de58
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-15 17:19:25 +0700

    Create MainViewModel.kt

---
+++ LatihanLiveData/app/src/main/java/com/erikriosetiawan/latihanlivedata/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.erikriosetiawan.latihanlivedata
+
+import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class MainViewModel : ViewModel() {
+
+    companion object {
+        private const val ONE_SECOND = 1000L
+    }
+
+    private val mInitialTime = SystemClock.elapsedRealtime()
+    private val mElapsedTime = MutableLiveData<Long?>()
+
+    init {
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+                mElapsedTime.postValue(newValue)
+            }
+        }, ONE_SECOND, ONE_SECOND)
+    }
+
+    fun getElapsedTime(): LiveData<Long?> = mElapsedTime
+}
