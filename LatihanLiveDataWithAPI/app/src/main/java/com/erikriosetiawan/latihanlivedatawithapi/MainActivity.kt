8c52f49bbe8468870b1cbba438724f989fed26b8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-16 19:03:30 +0700

    Integrated the view binding

--- LatihanLiveDataWithAPI/app/src/main/java/com/erikriosetiawan/latihanlivedatawithapi/MainActivity.kt
+++ LatihanLiveDataWithAPI/app/src/main/java/com/erikriosetiawan/latihanlivedatawithapi/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.erikriosetiawan.latihanlivedatawithapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.latihanlivedatawithapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
