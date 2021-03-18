b6bd7691b3645dc33c7aed7ed6ca5b52912da692
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-18 18:29:46 +0700

    Implements the espresso idling resource

--- LatihanIdlingResource/app/src/main/java/com/erikriosetiawan/latihanidlingresource/MainActivity.kt
+++ LatihanIdlingResource/app/src/main/java/com/erikriosetiawan/latihanidlingresource/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.erikriosetiawan.latihanidlingresource
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.latihanidlingresource.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            delay1()
+            delay2()
+        }
+    }
+
+    private fun delay1() {
+        EspressoIdlingResource.increment()
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.textView.text = getString(R.string.delay1)
+            if (!EspressoIdlingResource.getEspressoIdlingResource().isIdleNow) {
+                EspressoIdlingResource.decrement()
+            }
+        }, 2000)
+    }
+
+    private fun delay2() {
+        EspressoIdlingResource.increment()
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.textView.text = getString(R.string.delay2)
+            if (!EspressoIdlingResource.getEspressoIdlingResource().isIdleNow) {
+                EspressoIdlingResource.decrement()
+            }
+        }, 3000)
     }
 }
