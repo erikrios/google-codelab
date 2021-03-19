8bd6e90b70a76eebff942961c7d3a51804a65a11
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-19 10:34:27 +0700

    Integrated the view model

--- LatihanRoom/app/src/main/java/com/erikriosetiawan/latihanroom/ui/insert/NoteAddUpdateActivity.kt
+++ LatihanRoom/app/src/main/java/com/erikriosetiawan/latihanroom/ui/insert/NoteAddUpdateActivity.kt
@@ -5,6 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.latihanroom.R
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val REQUEST_ADD = 100
+        const val RESULT_ADD = 101
+        const val REQUEST_UPDATE = 200
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_add_update)
--- LatihanRoom/app/src/main/java/com/erikriosetiawan/latihanroom/ui/main/MainActivity.kt
+++ LatihanRoom/app/src/main/java/com/erikriosetiawan/latihanroom/ui/main/MainActivity.kt
@@ -1,12 +1,80 @@
 package com.erikriosetiawan.latihanroom.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.latihanroom.R
+import com.erikriosetiawan.latihanroom.database.Note
+import com.erikriosetiawan.latihanroom.databinding.ActivityMainBinding
+import com.erikriosetiawan.latihanroom.repository.ViewModelFactory
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateActivity
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateActivity.Companion.REQUEST_ADD
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateActivity.Companion.REQUEST_UPDATE
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateActivity.Companion.RESULT_ADD
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateActivity.Companion.RESULT_DELETE
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateActivity.Companion.RESULT_UPDATE
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this, noteObserver)
+
+        adapter = NoteAdapter(this@MainActivity)
+
+        binding.rvNotes.layoutManager = LinearLayoutManager(this)
+        binding.rvNotes.setHasFixedSize(true)
+        binding.rvNotes.adapter = adapter
+
+        binding.fabAdd.setOnClickListener { view ->
+            if (view.id == binding.fabAdd.id) {
+                val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+                startActivityForResult(intent, REQUEST_ADD)
+            }
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+
+    private val noteObserver = Observer<List<Note>> { noteList ->
+        noteList?.let {
+            adapter.setListNotes(it)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.let {
+            if (requestCode == REQUEST_ADD) {
+                if (resultCode == RESULT_ADD) {
+                    showSnackbarMessage(getString(R.string.added))
+                }
+            } else if (requestCode == REQUEST_UPDATE) {
+                if (resultCode == RESULT_UPDATE) {
+                    showSnackbarMessage(getString(R.string.changed))
+                } else if (resultCode == RESULT_DELETE) {
+                    showSnackbarMessage(getString(R.string.deleted))
+                }
+            }
+        }
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(binding.root as View, message, Snackbar.LENGTH_SHORT).show()
     }
 }
