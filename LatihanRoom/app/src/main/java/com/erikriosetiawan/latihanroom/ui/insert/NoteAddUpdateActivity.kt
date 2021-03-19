8134fd874e42cc442a30ae6528f3f160ee4215dd
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-19 09:20:33 +0700

    Create NoteAddUpdateActivity.kt

---
+++ LatihanRoom/app/src/main/java/com/erikriosetiawan/latihanroom/ui/insert/NoteAddUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.erikriosetiawan.latihanroom.ui.insert
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.latihanroom.R
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+    }
+}
