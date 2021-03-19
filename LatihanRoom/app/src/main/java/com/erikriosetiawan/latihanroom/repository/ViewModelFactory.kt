3f77f1eaca548b81b16d84928c0f2465488b1018
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-19 10:22:04 +0700

    Create ViewModelFactory.kt

---
+++ LatihanRoom/app/src/main/java/com/erikriosetiawan/latihanroom/repository/ViewModelFactory.kt
@@ -0,0 +1,39 @@
+package com.erikriosetiawan.latihanroom.repository
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateActivity
+import com.erikriosetiawan.latihanroom.ui.insert.NoteAddUpdateViewModel
+import com.erikriosetiawan.latihanroom.ui.main.MainViewModel
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory private constructor(private val mApplication: Application) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+
+        @JvmStatic
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE = ViewModelFactory(application)
+                    }
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(mApplication) as T
+        } else if (modelClass.isAssignableFrom(NoteAddUpdateActivity::class.java)) {
+            return NoteAddUpdateViewModel(mApplication) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
