be3a78a1ad3c90e4e56e8c2d14baf0a3c5d533f1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-21 09:50:21 +0700

    Create ApiService.kt

---
+++ DaggerHiltTutorial/app/src/main/java/com/erikriosetiawan/daggerhilttutorial/data/api/ApiService.kt
@@ -0,0 +1,11 @@
+package com.erikriosetiawan.daggerhilttutorial.data.api
+
+import com.erikriosetiawan.daggerhilttutorial.data.model.User
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("users")
+    suspend fun getUsers(): Response<List<User>>
+}
