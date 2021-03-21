f28d63188050a905f02a129c88dd0946510f3265
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-21 10:59:49 +0700

    Create MainAdapter.kt

---
+++ DaggerHiltTutorial/app/src/main/java/com/erikriosetiawan/daggerhilttutorial/ui/adapter/MainAdapter.kt
@@ -0,0 +1,43 @@
+package com.erikriosetiawan.daggerhilttutorial.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.erikriosetiawan.daggerhilttutorial.data.model.User
+import com.erikriosetiawan.daggerhilttutorial.databinding.ItemLayoutBinding
+
+class MainAdapter(
+    private val users: List<User>,
+    private var onItemClickListener: ((User) -> Unit)? = null
+) :
+    RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
+
+    fun setOnItemClickListener(listener: (User) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemLayoutBinding.inflate(layoutInflater)
+        return DataViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) =
+        holder.bind(users[position], onItemClickListener)
+
+    override fun getItemCount(): Int = users.size
+
+    class DataViewHolder(private val binding: ItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User, clickListener: ((User) -> Unit)?) {
+            binding.textViewUserName.text = user.name
+            binding.textViewUserEmail.text = user.email
+            Glide.with(binding.imageViewAvatar.context)
+                .load(user.avatar)
+                .into(binding.imageViewAvatar)
+
+            itemView.setOnClickListener { clickListener?.let { it(user) } }
+        }
+    }
+}
