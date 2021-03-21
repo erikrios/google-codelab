1e95dfc3481218393e781543aa19f48497587d71
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-21 11:58:29 +0700

    Integrated the view binding

--- DaggerHiltTutorial/app/src/main/java/com/erikriosetiawan/daggerhilttutorial/ui/main/view/MainActivity.kt
+++ DaggerHiltTutorial/app/src/main/java/com/erikriosetiawan/daggerhilttutorial/ui/main/view/MainActivity.kt
@@ -2,13 +2,17 @@ package com.erikriosetiawan.daggerhilttutorial.ui.main.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.erikriosetiawan.daggerhilttutorial.R
+import com.erikriosetiawan.daggerhilttutorial.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
