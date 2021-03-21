3982799690a7815f359f141cc62cea5ee9575105
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-21 11:56:21 +0700

    Create MainViewModel.kt

--- DaggerHiltTutorial/app/src/main/java/com/erikriosetiawan/daggerhilttutorial/ui/main/viewmodel/MainViewModel.kt
+++ DaggerHiltTutorial/app/src/main/java/com/erikriosetiawan/daggerhilttutorial/ui/main/viewmodel/MainViewModel.kt
@@ -1,4 +1,41 @@
 package com.erikriosetiawan.daggerhilttutorial.ui.main.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.erikriosetiawan.daggerhilttutorial.data.model.User
+import com.erikriosetiawan.daggerhilttutorial.data.repository.MainRepository
+import com.erikriosetiawan.daggerhilttutorial.utils.NetworkHelper
+import com.erikriosetiawan.daggerhilttutorial.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val mainRepository: MainRepository,
+    private val networkHelper: NetworkHelper
+) : ViewModel() {
+
+    private val _users = MutableLiveData<Resource<List<User>>>()
+    val users: LiveData<Resource<List<User>>>
+        get() = _users
+
+    init {
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        viewModelScope.launch {
+            _users.value = Resource.loading(null)
+            if (networkHelper.isNetworkConnected()) {
+                mainRepository.getUsers().let {
+                    if (it.isSuccessful) {
+                        _users.value = Resource.success(it.body())
+                    } else _users.value = Resource.error(it.errorBody().toString(), null)
+                }
+            } else _users.value = Resource.error("No internet connection", null)
+        }
+    }
 }
