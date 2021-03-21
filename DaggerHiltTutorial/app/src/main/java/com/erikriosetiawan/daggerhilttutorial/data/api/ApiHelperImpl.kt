cdb9d3c50e949ab57d2c55bdb23f8a0c9d1c34ba
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-21 09:53:46 +0700

    Create ApiHelperImpl.kt

---
+++ DaggerHiltTutorial/app/src/main/java/com/erikriosetiawan/daggerhilttutorial/data/api/ApiHelperImpl.kt
@@ -0,0 +1,10 @@
+package com.erikriosetiawan.daggerhilttutorial.data.api
+
+import com.erikriosetiawan.daggerhilttutorial.data.model.User
+import retrofit2.Response
+import javax.inject.Inject
+
+class ApiHelperImpl @Inject constructor(private val apiService: ApiService) : ApiHelper {
+
+    override suspend fun getUsers(): Response<List<User>> = apiService.getUsers()
+}
