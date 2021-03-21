d678f85b71d9664078f049b2ed95aa7b669fa937
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-21 19:25:03 +0700

    Create SimpleTextActivity.kt

---
+++ JetpackComposeAndroid/app/src/main/java/com/erikriosetiawan/jetpackcomposeandroid/text/SimpleTextActivity.kt
@@ -0,0 +1,15 @@
+package com.erikriosetiawan.jetpackcomposeandroid.text
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+class SimpleTextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
