19978c3fa6944aa77d2171799ddf561800b7a398
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-28 12:18:47 +0700

    Create a new activity

---
+++ MyModularizationApp/news_feature/src/main/java/com/erikriosetiawan/newsfeature/newsloader/NewsLoaderActivity.kt
@@ -0,0 +1,12 @@
+package com.erikriosetiawan.newsfeature.newsloader
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.newsfeature.R
+
+class NewsLoaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_loader)
+    }
+}
