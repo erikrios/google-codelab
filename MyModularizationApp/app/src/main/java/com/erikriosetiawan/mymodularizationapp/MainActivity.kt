4092f3cf8b8201bf5796c22ef3d62561981ec98d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-28 13:00:46 +0700

    Implements the dynamic module

--- MyModularizationApp/app/src/main/java/com/erikriosetiawan/mymodularizationapp/MainActivity.kt
+++ MyModularizationApp/app/src/main/java/com/erikriosetiawan/mymodularizationapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.erikriosetiawan.mymodularizationapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.mymodularizationapp.databinding.ActivityMainBinding
 import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 import com.google.android.play.core.splitinstall.SplitInstallRequest
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initDynamicModules()
+        setClickListener()
     }
 
     private fun initDynamicModules() {
@@ -31,12 +36,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setClickListener() {
-        binding.buttonClick.setOnClickListener {
-            if (!isDynamicFeatureDownloaded(DYNAMIC_FEATURE)) {
-                downloadFeature()
-            } else {
-                binding.buttonDeleteNewsModule.visibility = View.VISIBLE
-                binding.buttonOpenNewsModule.visibility = View.VISIBLE
+        binding.apply {
+            buttonClick.setOnClickListener {
+                if (!isDynamicFeatureDownloaded(DYNAMIC_FEATURE)) {
+                    downloadFeature()
+                } else {
+                    binding.buttonDeleteNewsModule.visibility = View.VISIBLE
+                    binding.buttonOpenNewsModule.visibility = View.VISIBLE
+                }
+            }
+            buttonOpenNewsModule.setOnClickListener {
+                val intent =
+                    Intent().setClassName(
+                        this@MainActivity,
+                        "com.eririosetiawan.newsfeature.NewsLoaderActivity"
+                    )
+            }
+            buttonDeleteNewsModule.setOnClickListener {
+                val list = mutableListOf<String>()
+                list.add(DYNAMIC_FEATURE)
+                uninstallDynamicFeature(list)
             }
         }
     }
@@ -46,13 +65,49 @@ class MainActivity : AppCompatActivity() {
 
     private fun downloadFeature() {
         splitInstallManager.startInstall(request)
-            .addOnFailureListener { }
+            .addOnFailureListener {
+                Log.d(MainActivity::class.java.simpleName, it.localizedMessage.toString())
+            }
             .addOnSuccessListener {
                 binding.apply {
+                    Log.d(MainActivity::class.java.simpleName, it.toString())
                     buttonOpenNewsModule.visibility = View.VISIBLE
                     buttonDeleteNewsModule.visibility = View.VISIBLE
                 }
             }
-            .addOnCompleteListener { }
+            .addOnCompleteListener {
+                Log.d(MainActivity::class.java.simpleName, it.result.toString())
+            }
+        monitorStateRequest()
+    }
+
+    private fun uninstallDynamicFeature(list: List<String>) {
+        splitInstallManager.deferredUninstall(list)
+            .addOnSuccessListener {
+                binding.apply {
+                    buttonOpenNewsModule.visibility = View.GONE
+                    buttonOpenNewsModule.visibility = View.GONE
+                }
+            }
+    }
+
+    private fun monitorStateRequest() {
+        var mySessionId = 0
+        val listener = SplitInstallStateUpdatedListener {
+            mySessionId = it.sessionId()
+            when (it.status()) {
+                DOWNLOADING -> {
+                    val totalBytes = it.totalBytesToDownload()
+                    val progress = it.bytesDownloaded()
+                }
+                INSTALLING -> Log.d("Status", "INSTALLING")
+                INSTALLED -> Log.d("Status", "INSTALLED")
+                FAILED -> Log.d("Status", "FAILED")
+                REQUIRES_USER_CONFIRMATION -> {
+                    Log.d("Status", "REQUIRES_USER_CONFIRMATION")
+                    startIntentSender(it.resolutionIntent()?.intentSender, null, 0, 0, 0)
+                }
+            }
+        }
     }
 }
