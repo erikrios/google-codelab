a801c084ffd157394106e0ffe0b5839edea92760
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-02 20:24:47 +0700

    Add debounce operator

--- LearnRxJavaAndRxAndroid/app/src/main/java/com/raywenderlich/android/cheesefinder/CheeseActivity.kt
+++ LearnRxJavaAndRxAndroid/app/src/main/java/com/raywenderlich/android/cheesefinder/CheeseActivity.kt
@@ -37,6 +37,7 @@ import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_cheeses.*
+import java.util.concurrent.TimeUnit
 
 class CheeseActivity : BaseSearchActivity() {
 
@@ -73,6 +74,7 @@ class CheeseActivity : BaseSearchActivity() {
         }
 
         return textChangeObservable.filter { it.length >= 2 }
+                .debounce(1000L, TimeUnit.MILLISECONDS)
     }
 
     @SuppressLint("CheckResult")
